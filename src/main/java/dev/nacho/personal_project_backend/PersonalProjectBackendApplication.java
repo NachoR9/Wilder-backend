982d5bce2e0fdf171d59fdda34d118aa58683ff1
NachoR9 <nachorm9@hotmail.com>
2024-09-15 19:06:20 -0700

    first commit

---
+++ src/main/java/dev/nacho/personal_project_backend/PersonalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.nacho.personal_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectBackendApplication.class, args);
+	}
+
+}
