f4a6177d54de80ac07463ee0aa5de316de6bee91
NachoR9 <nachorm9@hotmail.com>
2024-09-22 20:36:32 -0700

    videogame dto created

---
+++ src/main/java/dev/nacho/wilder/dtos/VideogameDto.java
@@ -0,0 +1,16 @@
+package dev.nacho.wilder.dtos;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class VideogameDto {
+    private Long id;
+    private String name;
+    private List<String> genre;
+    private LocalDate releaseDate;
+    private String company;
+    private String platform;
+}
