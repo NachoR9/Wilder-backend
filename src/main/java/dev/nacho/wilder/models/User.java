04c03b9dfecdacec4b13454e863ad6928410fa86
NachoR9 <nachorm9@hotmail.com>
2024-09-22 18:32:47 -0700

    h2 application properties

--- src/main/java/dev/nacho/wilder/models/User.java
+++ src/main/java/dev/nacho/wilder/models/User.java
@@ -1,10 +1,6 @@
 package dev.nacho.wilder.models;
 
 import java.util.Set;
-
-import org.springframework.context.annotation.Profile;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
