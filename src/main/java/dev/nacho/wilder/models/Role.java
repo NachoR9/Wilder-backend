73b932ca96f086fff6c017c426fc3f86647e2cfa
NachoR9 <nachorm9@hotmail.com>
2024-09-28 16:36:15 -0700

    auth controller and user dto created and login working

--- src/main/java/dev/nacho/wilder/models/Role.java
+++ src/main/java/dev/nacho/wilder/models/Role.java
@@ -1,15 +1,10 @@
 package dev.nacho.wilder.models;
 
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -24,9 +19,5 @@ public class Role {
     private Long id;
     @Column(name = "name")
     private String name;
-
-    @JsonIgnore
-    @ManyToMany(mappedBy = "roles")
-    Set<User> users;
 
 }
