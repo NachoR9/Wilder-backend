2ed53a4cda310375da4920bfac36c346bbb678f7
NachoR9 <nachorm9@hotmail.com>
2024-10-01 20:26:18 -0700

    fix logout

--- src/main/java/dev/nacho/wilder/config/SecurityConfig.java
+++ src/main/java/dev/nacho/wilder/config/SecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -45,7 +47,7 @@ public class SecurityConfig {
                         .formLogin(form -> form.disable())
                         .logout(out -> out
                                 .logoutUrl(endpoint + "/logout")
-                                .logoutSuccessUrl("http://localhost:5173/")
+                                .logoutSuccessHandler((new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)))
                                 .deleteCookies("JSESSIONID"))
                         .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
