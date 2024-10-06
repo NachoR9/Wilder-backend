13fd274542a529fa69eb5339dfaac89486fc6ed8
NachoR9 <nachorm9@hotmail.com>
2024-10-06 14:02:57 -0700

    add games to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,6 +11,41 @@ INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES
 INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Dragon age: The Veilguard','2024-10-31','Bioware','PC, PS5', 'https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fdragonageveilguard.png?alt=media');
 INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Dragon ball Sparking Zero','2024-10-11','Bandai Namco Entertainment','PC, PS5', 'https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fdragonballsparkingzero.png?alt=media');
 INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'New World Aeternum','2024-07-25','Amazon Game Studios','PS5', 'https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fnewworldaeternum.jpg?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Star Wars Outlaws', '2024-08-27','Ubisoft','PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fstar_wars_outlaws_.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'NBA 2K25', '2024-09-03','2K Games','PS5,Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fnba2k25.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Final Fantasy VII Rebirth','2024-02-29','Square Enix','PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fffvii%20rebirth.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Blasphemous 2','2023-08-24','Team 17','PS5, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fblasphemous%202.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Yu-Gi-Oh! Master Duel','2022-01-18','Konami','PS5, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fyu-gi-oh-master-duel-.jpg?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Astro Bot','2024-09-06','Team Asobi','PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2FAstro-Bot.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Rachet & Clank Rift Apart','2021-06-11','Insomniac Games','PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fratchet-and-clank.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Pokemon Scarlet','2022-11-18','The Pokemon Company','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2FPokemon_Scarlet_Logo.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Pokemon Violet','2022-11-18','The Pokemon Company','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fpokemon_violet_.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Super Mario Bros. Wonder','2023-10-20','Nintendo','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2FSuper_Mario_Bros._Wonder.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Tony Hawk''s Pro Skater 1+2','2020-09-04','Activision','PS5, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Ftony_hawk_1y2.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'The Quarry','2022-06-10','Supermassive Games','PC, PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fthe_quarry.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Marvel vs. Capcom Fighting Collection','2024-09-11','Capcom','PS4, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fmarvel_vs__capcom_fighting_collection.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Fire Emblem: Three Houses','2019-07-26','Nintendo', 'Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Ffire_emblem_three_houses.jpg?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Animal Crossing: New Horizons','2020-03-20','Nintendo','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2FAnimal_Crossing_NH.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Spider-Man 2','2023-10-20','Sony Interactive Entertainment','PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fspiderman2.jpeg?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Black Myth: Wukong','2024-08-19','Game Science','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fblack_myth_wukong.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'F1 24','2024-05-28','Electronic Arts','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Ff124.jpeg?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Prince of Persia: The Lost Crown','2024-01-18','Ubisoft','PS5,Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fprince_of_persia_lost_crown.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Tekken 8','2024-01-25','Bandai Namco Entertainment','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Ftekken8%20png.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Baldur''s Gate III','2023-08-03','Larian Studios','PC, PS5', 'https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fbaldurs_gate_III.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Bravely Default II','2021-02-26','Square Enix','PC, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fbravely_default_II.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'The Legend of Zelda: Tears of the Kingdom','2023-05-12','Nintendo','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fthe-legend-of-zelda-tears-of-the-kingdom.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Lies of P','2023-09-19','Fireshine Games','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Flies_of_pi.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Assassin''s Creed Valhalla','2020-11-10','Ubisoft','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fassassins_creed_valhalla.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Dragon Quest XI: Echoes of an Elusive Age','2017-07-29','Square Enix','PC,Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fdragon_quest_XI.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Wanted Raccoon','2021-03-19','MAD Sprouts','PC','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fwanted_raccoon.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'The Medium','2021-01-28','Bloober Team','PC, PS5, Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fthe_medium.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Street Fighter 6','2023-06-02','Capcom','PC,PS5','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2Fstreet_fighter_6.png?alt=media');
+INSERT INTO videogames (id, name, release_date, company, platform, image) VALUES (default, 'Xenoblade Chronicles 3','2022-07-29','Nintendo','Nintendo Switch','https://firebasestorage.googleapis.com/v0/b/wilder-app.appspot.com/o/public%2FXenoblade_3.png?alt=media');
+
+
+
+
+
 
 INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (1, 5);
 INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (1, 1);
@@ -18,6 +53,78 @@ INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (2, 5);
 INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (2, 1);
 INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (3, 2);
 INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (4, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (5, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (5, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (6, 6);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (6, 7);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (7, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (7, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (8, 3);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (9, 8);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (10, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (11, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (11, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (12, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (13, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (14, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (15, 6);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (15, 7);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (16, 3);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (17, 2);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (18, 8);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (19, 6);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (20, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (20, 2);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (20, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (21, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (21, 2);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (22, 7);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (23, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (23, 4);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (24, 2);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (25, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (25, 8);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (26, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (26, 8);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (27, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (28, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (29, 1);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (30, 5);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (31, 6);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (32, 3);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (33, 2);
+INSERT INTO videogames_genres (videogame_id, genres_id) VALUES (34, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* Roles */
 INSERT INTO roles (id, name) VALUES (default, 'ROLE_USER'); -- id = 1
